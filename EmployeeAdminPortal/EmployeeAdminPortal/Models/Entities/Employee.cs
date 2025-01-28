6c95d78b376643b0bc6c67fa0e39cbb33540a7e2
Prasamsha <prasamsha5555@gmail.com>
2025-01-28 11:38:43 +0545

    Push the EmployeeAdminPortal project

---
+++ EmployeeAdminPortal/EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeAdminPortal.Models.Entities
+{
+    public class Employee
+    {
+        // Globally Unique Identifier
+        public Guid Id { get; set; }
+        public  required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? Phone { get; set; }   // Question Mark makes this property nullable
+        public decimal Salary { get; set; }
+    }
+}
